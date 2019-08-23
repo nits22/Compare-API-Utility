517d4e6c8295622835b01f0fc9590f0902fab389
B0204885 <b0204885@Nitishs-Mac.local>
2019-08-23 11:35:05 +0530

    my first commit

---
+++ src/main/java/com/assignment/CompareApiUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompareApiUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompareApiUtilityApplication.class, args);
+	}
+
+}
