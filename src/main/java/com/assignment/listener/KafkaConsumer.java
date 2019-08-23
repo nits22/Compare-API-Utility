3f52d57bb86561207169547a9e472bb573929149
B0204885 <b0204885@Nitishs-Mac.local>
2019-08-23 13:59:19 +0530

    Added ExecutorService in KafkaConsumer to consume data from queue

--- src/main/java/com/assignment/listener/KafkaConsumer.java
+++ src/main/java/com/assignment/listener/KafkaConsumer.java
@@ -1,8 +1,12 @@
 package com.assignment.listener;
 
 import com.assignment.model.Requests;
+import com.assignment.utils.CompareUtility;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 ;
 
@@ -10,17 +14,27 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer {
 
 
-        @KafkaListener(topics = "Kafka_Example", group = "group_id")
-        public void consume(String message) {
-            System.out.println("Consumed message: " + message);
-        }
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
+
+    public void setExecutorService(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+    @KafkaListener(topics = "Kafka_Example", group = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
 
 
-        @KafkaListener(topics = "Kafka_Example_json", group = "group_json",
-                containerFactory = "userKafkaListenerFactory")
-        public void consumeJson(Requests requests) {
-            System.out.println("Consumed JSON Message: " + requests);
-        }
-
+    @KafkaListener(topics = "Kafka_json", group = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(Requests requests) {
+        System.out.println("Consumed JSON Message: " + requests);
+        executorService.execute(new CompareUtility(requests.getRequest1(), requests.getRequest2()));
+    }
 
 }
